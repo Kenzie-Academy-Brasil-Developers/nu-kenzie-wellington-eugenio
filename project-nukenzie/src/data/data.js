82dac144651a5d7ce20514add63c529aeb10d457
wellinton-eugenio <wellington.eliteinfo@gmail.com>
2022-11-21 02:01:00 -0300

    apllic responsivite

--- project-nukenzie/src/data/data.js
+++ project-nukenzie/src/data/data.js
@@ -9,6 +9,4 @@ export const categoriesData = [
     }
 ]
 
-export const transitionsData = [
-    
-]
+export const transitionsData = []
--- project-nukenzie/src/pages/homepage/index.jsx
+++ project-nukenzie/src/pages/homepage/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./style.css"
-import Illustration from './illustration.svg'
 
 const HomePage = ({setLogin}) => {
   return (
