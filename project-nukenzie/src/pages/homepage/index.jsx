c2e2bebbfedf43e464d1ade9108cdaf08dad9894
wellinton-eugenio <wellington.eliteinfo@gmail.com>
2022-11-21 02:31:47 -0300

    insert logoSVg

--- project-nukenzie/src/pages/homepage/index.jsx
+++ project-nukenzie/src/pages/homepage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from './logo.svg'
 import "./style.css"
 
 const HomePage = ({setLogin}) => {
   return (
     <div className='homeDiv'>
+      <div className='homecontainer'>
         <div className='homedesc'>
             <div className='logo'>
                 <h2 className='nu'>Nu</h2>
@@ -13,6 +15,8 @@ const HomePage = ({setLogin}) => {
             <p>de forma rápida e segura</p>
             <button className='acess' onClick={()=>setLogin(true)}>Acessar</button>
         </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
     </div>
   )
 }
